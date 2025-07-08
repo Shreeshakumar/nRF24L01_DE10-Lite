d949b3747ead14bbaaced8ecf5062782e18707ad
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2025-07-08 14:10:25 +0530

    Update spi_clock_divider.v

--- Design_01/spi_clock_divider.v
+++ Design_01/spi_clock_divider.v
@@ -1,23 +1,35 @@
-module spi_clock_divider #(
-    parameter DIVIDER = 5 // Generates 10MHz from 50MHz clk
-)(
+module spi_clock_divider (
     input wire clk_50,
     input wire rst,
     output reg clk_10
 );
 
-    reg [31:0] counter;
-
-    always @(posedge clk or posedge rst) begin
+    reg [31:0] counter_p;
+    reg [31:0] counter_n;
+    
+    always @(posedge clk_50 or posedge rst) begin
         if (rst) begin
-            counter <= 0;
+            counter_p <= 0;
             clk_10 <= 0;
         end else begin
-            if (counter == (DIVIDER - 1)) begin
+            if (counter_p == (5)) begin
                 clk_10 <= ~clk_10;
-                counter <= 0;
+                counter_p <= 0;
             end else begin
-                counter <= counter + 1;
+                counter_p <= counter_p + 1;
+            end
+        end
+    end
+
+    always @(negedge clk_50 or posedge rst) begin
+        if (rst) begin
+            counter_n <= 0;
+        end else begin
+            if (counter_n == (2)) begin
+                clk_10 <= ~clk_10;
+                counter_n <= 0;
+            end else begin
+                counter_n <= counter_n + 1;
             end
         end
     end
