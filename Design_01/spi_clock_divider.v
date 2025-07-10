34bd58f07092ebf0d8394ea03198599db66edf94
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2025-07-10 11:50:57 +0530

    Update spi_clock_divider.v

--- Design_01/spi_clock_divider.v
+++ Design_01/spi_clock_divider.v
@@ -1,18 +1,24 @@
 module spi_clock_divider (
     input wire clk_50,
     input wire rst,
-    output reg clk_10,
-    output reg [31:0] counter_p,
-    output reg [31:0] counter_n
+    output reg [2:0] counter_p,
+    output reg [2:0] counter_n,
+    output clk_10
+    //output reg p,
+    //output reg n
 );
-    
-    always @(posedge clk_50 or posedge rst) begin
+   // reg [31:0] counter_p;
+    //reg [31:0] counter_n;
+
+    //rst <= 1;
+    always @(posedge clk_50) begin
         if (rst) begin
             counter_p <= -1;
-            clk_10 <= 0;
+            //clk_10 <= 0;
+          //  p <= 0;
         end else begin
             if (counter_p == (4)) begin
-                clk_10 <= ~clk_10;
+                //clk_10 <= ~clk_10;
                 counter_p <= 0;
             end else begin
                 counter_p <= counter_p + 1;
@@ -20,17 +26,22 @@ module spi_clock_divider (
         end
     end
 
-    always @(negedge clk_50 or posedge rst) begin
+    always @(negedge clk_50) begin
         if (rst) begin
-            counter_n <= 2;
+            counter_n <= 0;
+           // n <= 0;
         end else begin
             if (counter_n == (4)) begin
-                clk_10 <= ~clk_10;
+               // clk_10 <= ~clk_10;
                 counter_n <= 0;
             end else begin
+                if (counter_p == 0) begin 
+                    counter_n <= 0; end
+                else begin
                 counter_n <= counter_n + 1;
-            end
+            end end
         end
     end
 
+    assign clk_10 = ((counter_p == (4)) |(counter_p == (3)) | (counter_n == (2)));
 endmodule
