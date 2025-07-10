9862c69da8c1ec7cbd6935044b908f8874751cf0
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2025-07-10 11:50:28 +0530

    Update spi_clock_divider_tb.v

--- Design_01/modules_tb/spi_clock_divider_tb.v
+++ Design_01/modules_tb/spi_clock_divider_tb.v
@@ -6,12 +6,18 @@ module spi_clock_divider_tb;
     reg clk_50;
     reg rst;
     wire clk_10;
+    wire [2:0] counter_p;
+    wire [2:0] counter_n;
 
     // Instantiate the module under test (MUT)
     spi_clock_divider #(.DIVIDER(5)) uut (
         .clk_50(clk_50),
         .rst(rst),
-        .clk_10(clk_10)
+        .clk_10(clk_10),
+        .counter_p(counter_p),
+        .counter_n(counter_n)
+      //  .p(p),
+        //.n(n)
     );
 
     // Generate 50 MHz clock (period = 20 ns)
